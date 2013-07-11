31a9ee385844731c2cfd2e510061a54a8b213ff3
Warren Chu <zhaoshouren@gmail.com>
2013-07-11 04:22:55 -0400

    Hello World from index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
