fa688bb72981a39c6b06e66a2ceab5fc7da2f3bc
Warren Chu <zhaoshouren@gmail.com>
2013-07-11 22:10:29 -0400

    adding HW3_2.txt and rolling back changes to web.js

--- web.js
+++ web.js
@@ -8,7 +8,6 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
